6dde0a96f6a4ff61dee47df70c696377885c539a
Murat Ozkan <murat.ozkan946@gmail.com>
2020-04-18 18:08:37 -0700

    Skeleton set of configs
    Not sure how this is going to take shape right now. Putting in some of
    the initial content.

---
+++ install/install_packages.sh
@@ -0,0 +1,18 @@
+#/bin/bash
+
+# Install packages
+# vim
+# powerline fonts
+# i3
+# rofi
+# firefox
+# feh
+# tree
+
+# Pull Git content
+# vundle
+# fonts
+# rofi themes
+
+# Configure applications
+# git - set name, e-mail, editor
